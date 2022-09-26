fcbb5408638acef1e69995e5773381520b7d693e
Filip Samól <114443811+Z3iTHx@users.noreply.github.com>
2022-09-26 23:34:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function checkMyGasPrice() {
+    if (document.querySelector("select").value == "Gas 95") {
+        let price = 6.15 * parseFloat(document.querySelector("#value").value)
+        document.querySelector("#show").innerHTML = price
+    } else if (document.querySelector("select").value == "Gas 98") {
+         let price = 6.55 * parseFloat(document.querySelector("#value").value)
+        document.querySelector("#show").innerHTML = price
+    } else if (document.querySelector("select").value == "Petrol") {
+        let price = 7.15 * parseFloat(document.querySelector("#value").value)
+        document.querySelector("#show").innerHTML = price
+    } else if (document.querySelector("select").value == "Verva Petrol") {
+        let price = 7.55 * parseFloat(document.querySelector("#value").value)
+        document.querySelector("#show").innerHTML = price
+    } else if (document.querySelector("select").value == "LPG") {
+        let price = 3.39 * parseFloat(document.querySelector("#value").value)
+        document.querySelector("#show").innerHTML = price
+    } else {
+        document.querySelector("#show").innerHTML = "Incorrect value"
+    }
+}
