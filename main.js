ca0c85a1d3af8b86149ef271f34ee31c000eb00e
Filip Samól <114443811+Z3iTHx@users.noreply.github.com>
2022-09-28 12:09:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 function checkMyGasPrice() {
     if (document.querySelector("select").value == "Gas 95") {
         let price = 6.15 * parseFloat(document.querySelector("#value").value)
-        document.querySelector("#show").innerHTML = price
+        document.querySelector("#show").innerHTML = price.toFixed(2
     } else if (document.querySelector("select").value == "Gas 98") {
          let price = 6.55 * parseFloat(document.querySelector("#value").value)
-        document.querySelector("#show").innerHTML = price
+        document.querySelector("#show").innerHTML = price.toFixed(2
     } else if (document.querySelector("select").value == "Petrol") {
         let price = 7.15 * parseFloat(document.querySelector("#value").value)
-        document.querySelector("#show").innerHTML = price
+        document.querySelector("#show").innerHTML = price.toFixed(2
     } else if (document.querySelector("select").value == "Verva Petrol") {
         let price = 7.55 * parseFloat(document.querySelector("#value").value)
-        document.querySelector("#show").innerHTML = price
+        document.querySelector("#show").innerHTML = price.toFixed(2
     } else if (document.querySelector("select").value == "LPG") {
         let price = 3.39 * parseFloat(document.querySelector("#value").value)
-        document.querySelector("#show").innerHTML = price
+        document.querySelector("#show").innerHTML = price.toFixed(2)
     } else {
         document.querySelector("#show").innerHTML = "Incorrect value"
     }
